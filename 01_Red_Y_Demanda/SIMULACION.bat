4251ebf713234de7cde36a34fed72f1b26d1b1f1
samuelmusa99 <samuelmusa99@gmail.com>
2026-06-03 20:49:43 -0400

    AGREGAR RESULTADOS PEROZO

--- 01_Red_Y_Demanda/SIMULACION.bat
+++ 01_Red_Y_Demanda/SIMULACION.bat
@@ -16,11 +16,11 @@ pause
 
 echo Convirtiendo resultados a CSV...
 
-cd /d "C:\Users\samue_yffy\Desktop\EXCEL RESULTADOS"
+cd /d "C:\Users\samue_yffy\Desktop\EXCEL RESULTADOS\PROPUESTA_PEROZO"
 
-python "C:\Users\samue_yffy\Desktop\EXCEL RESULTADOS" reporte_viajes.xml
-python "C:\Users\samue_yffy\Desktop\EXCEL RESULTADOS" reporte_colas.xml
-python "C:\Users\samue_yffy\Desktop\EXCEL RESULTADOS" reporte_densidad.xml
+python "C:\Users\samue_yffy\Desktop\EXCEL RESULTADOS\PROPUESTA_PEROZO" reporte_viajes.xml
+python "C:\Users\samue_yffy\Desktop\EXCEL RESULTADOS\PROPUESTA_PEROZO" reporte_colas.xml
+python "C:\Users\samue_yffy\Desktop\EXCEL RESULTADOS\PROPUESTA_PEROZO" reporte_densidad.xml
 
 echo.
 echo ¡Proceso terminado! Abriendo Excel de Análisis...
@@ -28,7 +28,7 @@ echo ¡Proceso terminado! Abriendo Excel de Análisis...
 echo ¡Proceso terminado! Abriendo Excel de Análisis...
 
 :: Abrir maximizado
-start /max excel "C:\Users\samue_yffy\Desktop\EXCEL RESULTADOS\RESULTADOS SIMULACION.xlsm"
+start /max excel "C:\Users\samue_yffy\Desktop\EXCEL RESULTADOS\PROPUESTA_PEROZO\RESULTADOS SIMULACION.xlsm"
 
 :: Cerramos la consola de inmediato para que Excel tome el control de la pantalla
 exit
